d005cf579f9c265254d1c0e4cdc871c956ae977d
Mochamad Rifki Bayu Destara <158830912+kikibades@users.noreply.github.com>
2024-03-11 02:57:36 +0700

    Update testagain.java

--- src/main/java/com/juaracoding/testagain.java
+++ src/main/java/com/juaracoding/testagain.java
@@ -8,7 +8,7 @@ public class testagain {
 
         for (int i = 0; i < m; i++) {
             for (int j = i + 1; j <= n - i; j++) {
-                // Ganti angka genap menjadi "_"
+                // ganti angka genap menjadi "_"
                 if (j % 2 == 0) {
                     System.out.print("_");
                 } else {
